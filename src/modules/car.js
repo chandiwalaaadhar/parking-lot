aa9284f1336e03ef62d8b9962d68fd7dec196056
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-04-09 08:43:34 +0530

    modules setup

---
+++ src/modules/car.js
@@ -0,0 +1,11 @@
+/**
+ * @description Car module
+ * @author Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
+ */
+
+class Car {
+  constructor(number, color) {
+    this.number = number;
+    this.color = color;
+  }
+}
