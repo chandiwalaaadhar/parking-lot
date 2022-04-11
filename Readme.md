fac7582e954ab93e450e0089bfc838a459ccfcda
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-04-11 18:18:07 +0530

    Added Test Command in Setup File and Added Readme

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Steps to Run The Project
+
+- From the Root Directory use `sudo sh bin/setup.sh`
+- Run the app using ` bin/parking_lot file.txt`
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 sudo apt install nodejs
 
 npm install
+
+npm test
